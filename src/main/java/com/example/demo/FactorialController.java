226e57b0076357cc92f7e50fb3344fa4bb239ad0
EunBongLee <eunbong73.lee@gmail.com>
2024-04-11 15:32:53 +0900

    Logging 라이브러리 수정

--- src/main/java/com/example/demo/FactorialController.java
+++ src/main/java/com/example/demo/FactorialController.java
@@ -1,12 +1,12 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
-import java.util.logging.Logger;
 
 @RestController
 public class FactorialController {
@@ -17,7 +17,7 @@ public class FactorialController {
         this.factorialService = factorialService;
     }
 
-    private final Logger logger = (Logger) LoggerFactory.getLogger(FactorialController.class);
+    private final Logger logger = LoggerFactory.getLogger(FactorialController.class);
 
     @GetMapping("/factorial")
     public BigDecimal factorial(@RequestParam ("n") int n ){
