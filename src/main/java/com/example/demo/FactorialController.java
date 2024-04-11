c6e1b5b653fbc1500f747a04a0ace6691cce6fc4
EunBongLee <eunbong73.lee@gmail.com>
2024-04-11 15:04:41 +0900

    Logging 추가

--- src/main/java/com/example/demo/FactorialController.java
+++ src/main/java/com/example/demo/FactorialController.java
@@ -1,10 +1,12 @@
 package com.example.demo;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
+import java.util.logging.Logger;
 
 @RestController
 public class FactorialController {
@@ -15,11 +17,15 @@ public class FactorialController {
         this.factorialService = factorialService;
     }
 
+    private final Logger logger = (Logger) LoggerFactory.getLogger(FactorialController.class);
+
     @GetMapping("/factorial")
     public BigDecimal factorial(@RequestParam ("n") int n ){
         if ( n < 0 )  {
             throw new ArithmeticException("n could be natural number");
         }
+
+        logger.info("factorial : "+n);
 
         return factorialService.calculate(n);
 
